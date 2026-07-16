eaa09be7ff34902e7916efa56b54bb8365b54018
Affan <snzaffan@gmail.com>
2026-07-17 02:41:01 +0600

    asfasfasf

--- Lab 4 Evalution/script.js
+++ Lab 4 Evalution/script.js
@@ -1,133 +1,157 @@
 let totalCount = 0;
-let mainList = [][];
-let subList = [];
-let index=0;
+let users = [];
 
-function validation() {
+let index=0;
+    let hasfname = true;
+    let haslname= true;
+    let hasidError = true;
+    let hasemail = true;
+    let hascredit = true;
+    let hasdept = true;
+ 
+function validationform() {
+ 
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
     const sid = document.getElementById("sid").value;
     const email = document.getElementById("email").value;
     const credit = document.getElementById("credit").value;
     const dept = document.getElementById("dept").value;
-
-    let fnameError = true;
-    let lnameError = true;
-    let idError = true;
-    let emailError = true;
-    let creditError = true;
-    let deptError = true;
-
   if (!fname) {
     document.getElementById("fnameError").innerHTML = "First Name can not be empty";
     document.getElementById("fnameError").style.color = "red";
-    fnameError = true;
+    hasfname = true;
   } 
   else {
     document.getElementById("fnameError").innerHTML = "";
-    fnameError = false;
-    student[0] = document.getElementById("fname").value;
+    hasfname = false;
+   
   }
   
 
   if (!lname) {
     document.getElementById("lnameError").innerHTML = "Last Name can not be empty";
     document.getElementById("lnameError").style.color = "red";
-    lameError = true;
+    haslname = true;
   } 
   else {
     document.getElementById("lnameError").innerHTML = "";
-    lameError = false;
-    student[1] = document.getElementById("lname").value;
+    haslname = false;
+    
   }
   
 
   if (!email) {
     document.getElementById("emailError").innerHTML =
       "Email is a required fireld";
-    document.getElementById("nameError").style.color = "red";
-    hasEmailError = true;
+    document.getElementById("emailError").style.color = "red";
+    hasemail = true;
   } 
   else if (!email.includes("@student.aiub.edu")) {
     document.getElementById("emailError").innerHTML =
       "Please provide a valid email address";
     document.getElementById("emailError").style.color = "red";
-    emailError = true;
+    hasemail = true;
   } else {
     document.getElementById("emailError").innerHTML = "";
-    emailError = false;
-    student[3] = document.getElementById("email").value;
+    hasemail = false;
+   
   }
 
 
   if (!sid) {
-    document.getElementById("sid").innerHTML =
+    document.getElementById("idError").innerHTML =
       "Student ID is a required field";
-    document.getElementById("sidError").style.color = "red";
-    sidlError = true;
+    document.getElementById("idError").style.color = "red";
+    hasidError = true;
   } 
   else if (!sid.includes("-")) {
-    document.getElementById("sidError").innerHTML =
+    document.getElementById("idError").innerHTML =
       "Please provide a valid ID";
-    document.getElementById("sidError").style.color = "red";
-    sidError = true;
+    document.getElementById("idError").style.color = "red";
+    hasidError = true;
   } else {
-    document.getElementById("emailError").innerHTML = "";
-    sidError = false;
-    student[2] = document.getElementById("sid").value;
+    document.getElementById("idError").innerHTML = "";
+    hasidError = false;
+   
   }
 
 
-  if (credit.length < 148 && credit.length>=0) {
+  if (credit>148 || credit<0 ) {
     document.getElementById("creditError").innerHTML =
-      "Name must be at least 3 char";
-    document.getElementById("creditError").style.color = "red";
-    creditError = true;
+      "Invalid Credit";
+   
+    hascredit = true;
   } else {
     document.getElementById("creditError").innerHTML = "";
-    creditError = false;
-    student[4] = document.getElementById("credit").value;
+    hascredit = false;
+    
   }
 
 
   if (!dept) {
     document.getElementById("deptError").innerHTML = "Department can not be empty";
-    document.getElementById("deptError").style.color = "red";
-    lameError = true;
+
+    hasdept = true;
   } 
   else {
     document.getElementById("deptError").innerHTML = "";
-    deptError = false;
-    student[5] = document.getElementById("dept").value;
+    hasdept = false;
+   
 
   }
 
-
-    
-  if (!fnameError && !lnameError && !idError && !emailError && !creditError && !deptError) {
-    document.getElementById("totalRegistrations").innerHTML = ++totalCount;
-
-    index= ++index;
-    mainList[index][0]= fname;
-    mainList[index][1]= lname;
-    mainList[index][2]= sid;
-    mainList[index][3]= email;
-    mainList[index][4]= credit;
-    mainList[index][5]= dept;
-  }
   
   
-displayTable();
+  return false;
 
-return false;
 }
 
-function displayTable(){
-  for(var i; i<index; i++){
-    for(var j; j<6; j++){
-      document.getElementById("tdfname").innerHTML = mainList[i][j];
+function displayinfo(){
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const sid = document.getElementById("sid").value;
+    const email = document.getElementById("email").value;
+    const credit = document.getElementById("credit").value;
+    const dept = document.getElementById("dept").value;
+     validationform(); 
+    if (!hasfname && !haslname && !hasidError && !hasemail && !hascredit && !hasdept) {
+    
+ 
+    users.push({
+        firstname: fname,
+        lasttname: lname,
+        id:sid,
+        emaill: email,
+        creditt: credit,
+        deptt:dept
+    });
+ 
+    let output = "";
+ 
+    for (let i = 0; i < users.length; i++) {
+ 
+        output += " "+ "<br>";
+        output += " "+ "<br>";
+        output += "FirstName : " + users[i].firstname + "<br>";
+        output += "LastName : " + users[i].lasttname + "<br>";
+        output += "Student ID : " + users[i].id + "<br>";
+        output += "Email : " + users[i].emaill + "<br>";
+        output += "Credit : " + users[i].creditt + "<br>";
+        output += "Dept: " + users[i].deptt + "<br>";
+       
     }
-    
-  }
-}
+ 
+    document.getElementById("display").innerHTML = output;
 
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("sid").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("credit").value = "";
+    document.getElementById("dept").selectedIndex = 0;
+}
+  
+  return false ;
+    
+}

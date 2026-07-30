8c970b3f08a1a8c7e6a8d49f5fe86f117138fa1b
Affan <snzaffan@gmail.com>
2026-07-30 13:52:37 +0600

    first php file

---
+++ Final/Lab_1/View/Task.php
@@ -0,0 +1,16 @@
+<?php
+$name= "Affan";
+$a=10;
+$b=20;
+
+$sum= $a + $b;
+    echo"<h1>Yo!! My name is AFFAN</h1>"
+?>
+
+
+<html>
+    <body>
+        <h1>My name is Affan. Yo!! </h1>
+    <body>
+
+<html>
